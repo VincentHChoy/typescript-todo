31e1f536cb7f823ed24763e4a5114349776fae35
VincentHChoy <vhchoy@ualberta.ca>
2022-10-31 15:19:24 -0600

    todo list finish

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,38 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const list = document.querySelector<HTMLUListElement>('#list');
+const form = document.querySelector('#new-task-form') as HTMLFormElement;
+const input = document.querySelector<HTMLInputElement>('#new-task-title');
 
-import confetti from 'canvas-confetti';
+type Task = {
+  id: string;
+  title: string;
+  completed: boolean;
+  createdAt: Date;
+};
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+form?.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (input?.value == '' || input?.value == null) return;
+
+  const newTask = {
+    id: '12345',
+    title: input.value,
+    completed: false,
+    createdAt: new Date(),
+  };
+
+  addListItem(newTask);
+  input.value = '';
+});
+
+const addListItem = (task: Task) => {
+  const item = document.createElement('li');
+  const label = document.createElement('label');
+  const checkbox = document.createElement('input');
+
+  checkbox.type = 'checkbox';
+  checkbox.checked = task.completed
+  label.append(checkbox,task.title)
+  item.append(label);
+  list?.append(item);
+  
+};
